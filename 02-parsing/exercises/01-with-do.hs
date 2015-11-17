cb0dd9b0e80629abc2b98eb8a8bab7351a44056c
M. Maxwell Watson <hi@mmwtsn.com>
2015-11-17 18:50:15 -0500

    Implement parseNumber with do notation

---
+++ 02-parsing/exercises/01-with-do.hs
@@ -0,0 +1,53 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+data LispVal = Atom String
+    | List [LispVal]
+    | DottedList [LispVal] LispVal
+    | Number Integer
+    | String String
+    | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first:rest
+    return $ case atom of
+        "#t" -> Bool True
+        "#f" -> Bool False
+        _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = do
+    x <- many1 digit
+    return $ Number $ read x
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+    <|> parseString
+    <|> parseNumber
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right _ -> "Found value"
+
+main :: IO ()
+main = do
+    (expr:_) <- getArgs
+    putStrLn $ readExpr expr
